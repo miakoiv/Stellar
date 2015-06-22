39525a9b778782312a21da726c21fce3662a515f
Sami Rosenblad <rosenblad@gmail.com>
2015-06-22 13:19:03 +0300

    Added brand reference to users to look up order types by brand

--- db/migrate/20150615132455_devise_create_users.rb
+++ db/migrate/20150615132455_devise_create_users.rb
@@ -1,6 +1,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.belongs_to :brand, null: false, index: true
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
