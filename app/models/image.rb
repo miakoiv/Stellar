90eba633aedb9faaef5bc04bcce7d6be257a4339
Sami Rosenblad <rosenblad@gmail.com>
2015-06-23 16:37:21 +0300

    Revised image attachment sizes

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,7 +5,11 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
   belongs_to :image_type
   has_attached_file :attachment,
-    styles: {large: '1024x1024>', medium: '256x256>', thumb: '64x64>'}
+    styles: {
+      lightbox: '1024x1024>',
+      thumbnail: '128x128>',
+      icon: '32x32>',
+    }
 
 
   validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\Z/
