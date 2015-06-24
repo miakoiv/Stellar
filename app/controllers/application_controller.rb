bf17bce0035298fc68b8dad684f4bbe279f25534
Sami Rosenblad <rosenblad@gmail.com>
2015-06-24 13:31:51 +0300

    Added current_brand helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
 
   # Authenticate every action.
   before_action :authenticate_user!
+
+  # Find the current brand for the storefront section
+  # that is restricted to a single brand.
+  def current_brand
+    if params[:brand_id].present? # and admin?
+      session[:brand_id] = params[:brand_id]
+    end
+    if session[:brand_id].present?
+      Brand.find(session[:brand_id])
+    else
+      current_user.brand
+    end
+  end
+  helper_method :current_brand
 end
