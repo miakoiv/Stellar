01b8d3712903f519d5c6d160f146225b57bcdc02
Sami Rosenblad <rosenblad@gmail.com>
2015-06-24 14:07:34 +0300

    Beautification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
 
-  # Find an icon for given object.
+  # Display an icon for object.
   def icon_image(object)
     return nil if object.images.icons.empty?
     image_tag(object.images.icons.first.attachment.url(:icon))
