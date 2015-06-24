ecdcb38e6362e59c2c5c0f37bb9721cd8fd8fcb2
Sami Rosenblad <rosenblad@gmail.com>
2015-06-24 09:22:36 +0300

    Added icon_image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,12 @@
 
 module ApplicationHelper
 
+  # Use the first image of any object as its icon.
+  def icon_image(object)
+    return nil if object.images.empty?
+    image_tag(object.images.first.attachment.url(:icon))
+  end
+
   def blank_option
     "\u274c" # U+274C CROSS MARK
   end
