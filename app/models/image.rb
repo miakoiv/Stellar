afa0ebd6a03325fa208b924e3a5c4b99faa29eab
Sami Rosenblad <rosenblad@gmail.com>
2015-07-01 11:18:25 +0300

    Added support for non-bitmap attachments, intelligently assign image type

--- app/models/image.rb
+++ app/models/image.rb
@@ -23,11 +23,38 @@ class Image < ActiveRecord::Base
       thumbnail: '-strip -quality 70',
       icon: '-strip -quality 70',
     }
+  before_post_process :resize_bitmaps
+  before_create :assign_image_type
 
   scope :by_type, -> (type) { joins(:image_type).where(image_types: {name: type}) }
 
   delegate :url, to: :attachment
 
-  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\Z/
+  validates_attachment :attachment,
+    content_type: {
+      content_type: [
+        %r{\Aimage/(bmp|jpeg|jpg|png|x-png)},
+        %r{\Aapplication/(pdf|msword)},
+        %r{\aapplication/vnd.openxmlformats},
+      ]
+    }
 
+  # Applicable image types due to attachment bitmappiness.
+  def applicable_image_types
+    ImageType.where(bitmap: is_bitmap?)
+  end
+
+  # Assign first applicable image type.
+  def assign_image_type
+    self.image_type = applicable_image_types.first
+  end
+
+  def is_bitmap?
+    !!(attachment_content_type =~ /\Aimage/)
+  end
+
+  private
+    def resize_bitmaps
+      return false unless is_bitmap?
+    end
 end
