aa8edc1ecc4ce5ba30a29300d44d5ba3b763d818
Sami Rosenblad <rosenblad@gmail.com>
2015-07-02 17:24:24 +0300

    Added abilities to edit and destroy order items

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,33 @@
+#encoding: utf-8
+
+class OrderItemsController < ApplicationController
+
+  # PATCH/PUT /order_items/1
+  def update
+    @order_item = OrderItem.find(params[:id])
+
+    respond_to do |format|
+      if @order_item.update(order_item_params)
+        format.js
+      end
+    end
+  end
+
+  # DELETE /order_items/1
+  def destroy
+    @order_item = OrderItem.find(params[:id])
+
+    respond_to do |format|
+      if @order_item.destroy
+        format.js
+      end
+    end
+  end
+
+  private
+    def order_item_params
+      params.require(:order_item).permit(
+        :amount
+      )
+    end
+end
