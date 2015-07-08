362e6df49dd9e5c00d0ded388e5ee87bd87ffd80
Sami Rosenblad <rosenblad@gmail.com>
2015-07-08 16:11:23 +0300

    Disabled creation of orders, only shopping carts will be promoted to orders

--- app/authorizers/order_authorizer.rb
+++ app/authorizers/order_authorizer.rb
@@ -3,9 +3,6 @@
 class OrderAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
-    user.is_site_manager?  ||
-    user.is_store_manager? ||
-    user.is_sales_rep?     ||
     false
   end
 
