07964570c75d24e961df8861f4f43fb171bb0b62
Sami Rosenblad <rosenblad@gmail.com>
2015-07-09 09:38:58 +0300

    Added order controller actions to edit and destroy orders, order items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,28 @@
 
 class OrdersController < ApplicationController
 
+  before_action :set_order, only: [:edit, :destroy]
+
   # GET /orders
   def index
     @orders = current_user.orders.unapproved
   end
 
+  # GET /orders/edit/1
+  def edit
+  end
+
+  # DELETE /orders/1
+  def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_path, notice: 'Order deleted.'}
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
 end
