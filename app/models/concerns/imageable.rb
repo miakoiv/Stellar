26f596e19047ca0a8c8e7faad5b2aa140d706f13
Sami Rosenblad <rosenblad@gmail.com>
2015-07-16 15:10:22 +0300

    Made images dependent of parent

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -2,7 +2,7 @@ module Imageable
   extend ActiveSupport::Concern
 
   included do
-    has_many :images, as: :imageable
+    has_many :images, as: :imageable, dependent: :destroy
   end
 
   # Use the first presentational image as cover image.
