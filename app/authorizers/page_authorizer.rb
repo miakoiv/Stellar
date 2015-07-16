f431ab2199e0214233427ee710efd8882400a91d
Sami Rosenblad <rosenblad@gmail.com>
2015-07-16 17:37:38 +0300

    Allowed store managers to edit pages

--- app/authorizers/page_authorizer.rb
+++ app/authorizers/page_authorizer.rb
@@ -4,21 +4,25 @@ class PageAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
     user.is_site_manager?  ||
+    user.is_store_manager? ||
     false
   end
 
   def self.readable_by?(user)
     user.is_site_manager?  ||
+    user.is_store_manager? ||
     false
   end
 
   def self.updatable_by?(user)
     user.is_site_manager?  ||
+    user.is_store_manager? ||
     false
   end
 
   def self.deletable_by?(user)
     user.is_site_manager?  ||
+    user.is_store_manager? ||
     false
   end
 
