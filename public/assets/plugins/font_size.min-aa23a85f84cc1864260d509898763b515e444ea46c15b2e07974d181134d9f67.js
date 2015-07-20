f91516159b7ac8d4520863c19b8dc3c34179a065
Sami Rosenblad <rosenblad@gmail.com>
2015-07-20 09:14:46 +0300

    Fixed asset confusion - froala themes conflicted with our own themes

---
+++ public/assets/plugins/font_size.min-aa23a85f84cc1864260d509898763b515e444ea46c15b2e07974d181134d9f67.js
@@ -0,0 +1,6 @@
+/*!
+ * froala_editor v1.2.8 (https://www.froala.com/wysiwyg-editor)
+ * License https://www.froala.com/wysiwyg-editor/terms
+ * Copyright 2014-2015 Froala Labs
+ */
+!function(e){e.Editable.prototype.refreshFontSize=function(){var t=e(this.getSelectionElement()),i=parseInt(t.css("font-size").replace(/px/g,""),10)||16;this.$editor.find('.fr-dropdown > button[data-name="fontSize"] + ul li').removeClass("active"),this.$editor.find('.fr-dropdown > button[data-name="fontSize"] + ul li[data-val="'+i+'px"]').addClass("active")},e.Editable.commands=e.extend(e.Editable.commands,{fontSize:{title:"Font Size",icon:"fa fa-text-height",refreshOnShow:e.Editable.prototype.refreshFontSize,seed:[{min:11,max:52}],undo:!0,callback:function(e,t){this.inlineStyle("font-size",e,t)},callbackWithoutSelection:function(e,t){this._startInFontExec("font-size",e,t)}}}),e.Editable.prototype.command_dispatcher=e.extend(e.Editable.prototype.command_dispatcher,{fontSize:function(e){var t=this.buildDropdownFontsize(e),i=this.buildDropdownButton(e,t);return i}}),e.Editable.prototype.buildDropdownFontsize=function(e){for(var t='<ul class="fr-dropdown-menu f-font-sizes">',i=0;i<e.seed.length;i++)for(var n=e.seed[i],o=n.min;o<=n.max;o++)t+='<li data-cmd="'+e.cmd+'" data-val="'+o+'px"><a href="#"><span>'+o+"px</span></a></li>";return t+="</ul>"}}(jQuery);
