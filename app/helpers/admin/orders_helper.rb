e4982a87ccf1a53d7aeaf7d12924d6e8675b0735
Sami Rosenblad <rosenblad@gmail.com>
2015-07-22 09:49:17 +0300

    Made tabs sticky, added anchors to 'back' links to point at the correct tab

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,2 +1,9 @@
+#encoding: utf-8
+
 module Admin::OrdersHelper
+
+  # Returns the name of the tab order resides on in order index.
+  def orders_tab(order)
+    order.approval ? 'approved' : ''
+  end
 end
--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,9 @@
+#encoding: utf-8
+
 module Admin::ProductsHelper
+
+  # Returns the name of the tab product resides on in product index.
+  def products_tab(product)
+    product.category.nil? ? 'uncategorized' : ''
+  end
 end
