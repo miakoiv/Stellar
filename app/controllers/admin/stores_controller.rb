20841c1e95e8c6a68173fc7b442a349aac44aa6d
Sami Rosenblad <rosenblad@gmail.com>
2015-08-13 13:58:06 +0300

    Included the host field in store show and edit views

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -67,7 +67,7 @@ class Admin::StoresController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def store_params
       params.require(:store).permit(
-        :contact_person_id, :erp_number, :name, :theme
+        :contact_person_id, :erp_number, :name, :host, :theme
       )
     end
 end
