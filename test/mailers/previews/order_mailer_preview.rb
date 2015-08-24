37321a95dc8152cd8ba7e9dd1c1123d474863edf
Sami Rosenblad <rosenblad@gmail.com>
2015-08-24 15:41:13 +0300

    Updated logo, added Zurb Ink styles, cleaned up old assets

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,11 @@
+#encoding: utf-8
+
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
+
 class OrderMailerPreview < ActionMailer::Preview
+
+  def order_confirmation
+    OrderMailer.order_confirmation(Order.first)
+  end
 
 end
