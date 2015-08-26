f2a2640a059cead3944fdae8e8186b3ff2f15265
Sami Rosenblad <rosenblad@gmail.com>
2015-08-26 16:32:01 +0300

    All products now displayed as a category. UI and theme improvements

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,10 +1,4 @@
 #encoding: utf-8
 
 module StoreHelper
-
-  def link_to_all_products
-    content_tag(:div, nil, class: 'corner') +
-    link_to('★', show_all_products_path, class: 'corner-link')
-  end
-
 end
