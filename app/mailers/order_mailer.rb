8b9c406b9727ef0115988eb254237b6e578974ac
Sami Rosenblad <rosenblad@gmail.com>
2015-09-23 16:42:01 +0300

    Re-enabled mailing carbon copies of order confirmations

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -13,7 +13,7 @@ class OrderMailer < ApplicationMailer
     roadie_mail(
       from: @store.contact_person.to_s,
       to: @user.to_s,
-      #cc: @store.contact_person.to_s,
+      cc: @store.contact_person.to_s,
       subject: default_i18n_subject(store: @store)
     )
   end
