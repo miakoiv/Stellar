8d71d140732b5728bf4a77d069ac3a005e84aa87
Sami Rosenblad <rosenblad@gmail.com>
2015-10-22 14:30:06 +0300

    Validations and views for the Get One Free promotion handler

--- app/models/promotion_get_one_free.rb
+++ app/models/promotion_get_one_free.rb
@@ -2,4 +2,13 @@
 
 class PromotionGetOneFree < PromotionHandler
 
+  validates :required_items,
+    numericality: {only_integer: true, greater_than: 1},
+    on: :update
+  validates :discount_percent,
+    numericality: {
+      only_integer: true,
+      greater_than: 0,
+      less_than_or_equal_to: 100
+    }, on: :update
 end
