338abbe476d9cef10173400c10523f4f3a859d02
Sami Rosenblad <rosenblad@gmail.com>
2015-10-29 10:13:47 +0200

    Fixed erroneous tab names or orders and product helpers

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -4,6 +4,6 @@ module Admin::ProductsHelper
 
   # Returns the name of the tab product resides on in product index.
   def products_tab(product)
-    product.category.present? ? dom_id(product.category) : 'uncategorized'
+    product.category.present? ? "tab-#{dom_id(product.category)}" : 'tab-uncategorized'
   end
 end
