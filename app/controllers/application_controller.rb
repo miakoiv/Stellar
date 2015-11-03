20ed15585ef8e560a67386e263a390d45c0814c1
Sami Rosenblad <rosenblad@gmail.com>
2015-11-03 15:15:27 +0200

    Restructured application controller, added some controller lifetime scoped caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Pages must be available to main navigation at all times.
+  #---
   before_action :load_pages
-  def load_pages
-    @pages ||= current_store.pages.top_level.ordered
-  end
+  before_action :set_locale
+  after_filter :prepare_unobtrusive_flash
 
+  #---
   # Authenticate user, but skip authentication
   # if the current store admits guests.
   def authenticate_user_or_skip!
@@ -26,62 +26,63 @@ class ApplicationController < ActionController::Base
       alert: 'You are not authorized to complete that action.'
   end
 
-  before_action :set_locale
-  def set_locale
-    I18n.locale = params[:locale] || current_store.locale || I18n.default_locale
-  end
-
-  after_filter :prepare_unobtrusive_flash
-
-  # Set current store to the current user's store. For unauthenticated guests,
-  # use the store matching requested hostname.
-  def current_store
-    @current_store ||= user_signed_in? && current_user.store || current_store_by_request
-  end
-  helper_method :current_store
-
-  # Convenience method to access the current user's shopping cart.
-  def shopping_cart
-    @shopping_cart ||= current_user.shopping_cart(current_store)
-  end
-  helper_method :shopping_cart
-
-  # Can the current user perform shopping at current store?
-  def can_shop?
-    current_user.can?(:shop, store: current_store)
-  end
-  helper_method :can_shop?
-
-  # Can the current user see pricing?
-  def can_see_pricing?
-    current_user.has_role?(:see_pricing)
-  end
-  helper_method :can_see_pricing?
-
-  # Can the current user see stock numbers?
-  def can_see_stock?
-    current_user.has_role?(:see_stock)
-  end
-  helper_method :can_see_stock?
-
-  # Can the current user manage her store?
-  def can_manage?
-    current_user.has_role?(:manager)
-  end
-  helper_method :can_manage?
-
-  # Can the current user access the dashboard at her store?
-  def can_access_dashboard?
-    current_user.has_role?(:dashboard_access)
-  end
-  helper_method :can_access_dashboard?
-
   # Find the guest user stored in session, or create it.
   def guest_user
     @cached_guest ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
   end
 
+  # The methods below are for convenience and to cache often repeated
+  # database queries on current user and her roles.
+  helper_method :current_store, :shopping_cart, :can_shop?, :can_see_pricing?, :can_see_stock?, :can_manage?, :can_access_dashboard?
+
+  def current_store
+    @current_store ||= user_signed_in? && current_user.store || current_store_by_request
+  end
+
+  def shopping_cart
+    @shopping_cart ||= current_user.shopping_cart(current_store)
+  end
+
+  def can_shop?
+    @can_shop = current_user.can?(:shop, store: current_store) if @can_shop.nil?
+    @can_shop
+  end
+
+  def can_see_pricing?
+    @can_see_pricing = current_user.has_role?(:see_pricing) if @can_see_pricing.nil?
+    @can_see_pricing
+  end
+
+  def can_see_stock?
+    @can_see_stock = current_user.has_role?(:see_stock) if @can_see_stock.nil?
+    @can_see_stock
+  end
+
+  def can_manage?
+    @can_manage = current_user.has_role?(:manager) if @can_manage.nil?
+    @can_manage
+  end
+
+  def can_access_dashboard?
+    @can_access_dashboard = current_user.has_role?(:dashboard_access) if @can_access_dashboard.nil?
+    @can_access_dashboard
+  end
+
   private
+    # Pages are loaded by a before_filter, since their links are
+    # rendered in the main navigation bar.
+    def load_pages
+      @pages ||= current_store.pages.top_level.ordered
+    end
+
+    # Locale is set by a before_filter. Using params is a manual override
+    # not available in the user interface.
+    def set_locale
+      I18n.locale = params[:locale] || current_store.locale || I18n.default_locale
+    end
+
+    # When no user is signed in, or a guest user is created, the current store
+    # is looked up by the requested hostname.
     def current_store_by_request
       Store.find_by(host: request.host)
     end
