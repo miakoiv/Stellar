1fb961ca008dc7e7fd4f17811d92a17d0e6cafbf
Sami Rosenblad <rosenblad@gmail.com>
2015-11-03 16:27:12 +0200

    Whitelisted the local IP address block to the web console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Whitelist our virtual IP address block to the web console.
+  config.web_console.whitelisted_ips = '10.2.0.0/16'
 end
