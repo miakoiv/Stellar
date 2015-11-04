b650e9d0e63da1331ba4a995ccebac03c8481b8e
Sami Rosenblad <rosenblad@gmail.com>
2015-11-04 11:27:24 +0200

    Added current store scope

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -68,7 +68,7 @@ class Admin::OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = current_store.orders.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
