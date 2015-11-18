88817d2d4f58560376a0c1843dbd4896f20c0653
Sami Rosenblad <rosenblad@gmail.com>
2015-11-18 14:41:20 +0200

    Fresh assets

---
+++ public/assets/spry_themes/lepola-1dc796a911e3446925a57943cf438d4acd451db59ccf55701d90ddaadbe619a4.js
@@ -0,0 +1,1 @@
+(function(){jQuery(function(){var t;return t=$(".categories").height()+10,$(".products").css("padding-top",t),$(".product-detail").css("padding-top",t)})}).call(this);
--- public/assets/spry_themes/lepola-b417b1d4280618f4c0614df426d5c8379c0dae88d7b1b8b9e09f368a8ad04d72.js
+++
@@ -1,1 +0,0 @@
-(function(){jQuery(function(){return console.log($("ul.categories").first)})}).call(this);
