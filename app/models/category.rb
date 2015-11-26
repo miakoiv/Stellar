bc254d2994e8d76afa9c746b23cce4dd43a2081a
Sami Rosenblad <rosenblad@gmail.com>
2015-11-26 15:39:54 +0200

    Improved rendering of categories and subcategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -21,10 +21,19 @@ class Category < ActiveRecord::Base
   validates :name, presence: true
 
   #---
+  # Category is inside another category if it's the category itself,
+  # or inside one of its parent categories.
+  def inside?(category)
+    category == self || parent_category.present? && parent_category.inside?(category)
+  end
+
   def top_level
     parent_category.nil? ? self : parent_category.top_level
   end
 
+  # If this category has no products, tries the first subcategory.
+  # Defaults to self if there are no subcategories, or the first
+  # subcategory has no products either.
   def having_products
     return self if sub_categories.empty? || products.any?
     sub_categories.first.products.empty? ? self : sub_categories.first
