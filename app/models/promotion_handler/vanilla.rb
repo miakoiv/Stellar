142ed30ca8995c56bb8a6a08f1693a13b9c67498
Sami Rosenblad <rosenblad@gmail.com>
2016-03-01 14:37:10 +0200

    Made vanilla promotionhandler's apply method a no-op, since promoted prices are applied directly

--- app/models/promotion_handler/vanilla.rb
+++ app/models/promotion_handler/vanilla.rb
@@ -3,15 +3,9 @@
 class PromotionHandler
   class Vanilla < PromotionHandler
 
+    # Vanilla promotions do not apply adjustments since order items
+    # are created with the promoted price.
     def apply!(order_items)
-      order_items.each do |order_item|
-        promoted_item = promotion.item_from_order_item(order_item)
-        order_item.adjustments.create(
-          source: promoted_item,
-          label: promoted_item.description,
-          amount: order_item.amount * (promoted_item.price - order_item.price)
-        )
-      end
     end
 
     def editable_prices?
