8fcfe65c06db667aedf7ebb6b59161dd8c814b91
Sami Rosenblad <rosenblad@gmail.com>
2016-03-14 16:09:31 +0200

    Allow editing order items of any order, only permit changing the price on completed orders

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,12 @@
 #encoding: utf-8
-
+#
+# Order items have a dualist nature as shopping cart items before they
+# are part of a completed order. If the item being updated is part of
+# an incomplete order, @is_cart will be set, and permitted parameters
+# restricted to amounts only.
+# Rendering responses to AJAX requests looks at this variable to render
+# appropriate updates to the DOM.
+#
 class OrderItemsController < ApplicationController
 
   # This controller is aware of unauthenticated guests.
@@ -29,13 +36,13 @@ class OrderItemsController < ApplicationController
 
   private
     def set_order_and_item
-      @order = shopping_cart
-      @order_item = @order.order_items.find(params[:id])
+      @order_item = OrderItem.find(params[:id])
+      @order = @order_item.order
+      @is_cart = !@order.complete?
     end
 
     def order_item_params
-      params.require(:order_item).permit(
-        :amount
-      )
+      permitted = @is_cart ? [:amount] : [:amount, :price]
+      params.require(:order_item).permit(*permitted)
     end
 end
