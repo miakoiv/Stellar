miakoiv/Stellar

commit 3ef12838a6216ade37db7f00c813a0dd6982c9ac

Added logic to have products touch themselves when their category assignments change

app/models/product.rb
@@ -33,7 +33,10 @@ class Product < ActiveRecord::Base
 
   #---
   belongs_to :store
-  has_and_belongs_to_many :categories
+
+  # A product may belong to multiple categories, and must update its own
+  # timestamp when the relationships change.
+  has_and_belongs_to_many :categories, after_add: :touch_self, after_remove: :touch_self
 
   has_many :inventory_items, -> (product) {
     joins(:product).where('products.store_id = inventory_items.store_id')
@@ -179,6 +182,10 @@ class Product < ActiveRecord::Base
   end
 
   protected
+    def touch_self
+      self.touch if persisted?
+    end
+
     def touch_categories
       categories.each(&:touch)
     end

commit 61b9196d5bc37f0a50e6e6f3f21187f03ebeb972

Fixed bug introduced in 3ef12838: callback was missing an argument

app/models/product.rb
@@ -182,7 +182,7 @@ class Product < ActiveRecord::Base
   end
 
   protected
-    def touch_self
+    def touch_self(category)
       self.touch if persisted?
     end
 