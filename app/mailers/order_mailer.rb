c2c0d06d8497d03e3a0d41c6e1b7a9c9a60ab315
Sami Rosenblad <rosenblad@gmail.com>
2016-04-20 09:33:59 +0300

    Improved order mailer, suppressed sending carbon copies of quotations

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,31 +5,38 @@ class OrderMailer < ApplicationMailer
   include Roadie::Rails::Mailer
 
   #---
+  # Order confirmation for the customer. Carbon copies are sent to the users
+  # responsible for managing this order, unless it's a quotation.
   def order_confirmation(order)
     @order = order
     @store = order.store
     @user = order.user
 
-    roadie_mail(
+    headers = {
       from: "noreply@#{@store.host}",
       to: "#{@order.customer_name} <#{@order.customer_email}>",
-      cc: @order.managing_users.map(&:to_s),
       subject: default_i18n_subject(store: @store)
-    )
+    }
+    unless @order.is_quote?
+      headers.merge!(cc: @order.managing_users.map(&:to_s))
+    end
+    roadie_mail(headers)
   end
 
+  # Quotation from the user to the specified contact, blind carbon copied
+  # to the sender.
   def quotation(order)
-    Rails.logger.info "#{order.inspect}"
     @order = order
     @store = order.store
     @user = order.user
 
-    roadie_mail(
+    headers = {
       from: @order.user.to_s,
       to: "#{@order.contact_person} <#{@order.contact_email}>",
       bcc: @order.user.to_s,
       subject: default_i18n_subject(store: @store)
-    )
+    }
+    roadie_mail(headers)
   end
 
   protected
