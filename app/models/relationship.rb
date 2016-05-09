47ed739c421340c0cd9f0b6c1280a4bddf351a9f
Sami Rosenblad <rosenblad@gmail.com>
2016-05-09 11:45:34 +0300

    Added validations to relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,4 +5,7 @@ class Relationship < ActiveRecord::Base
   belongs_to :product, touch: true
   belongs_to :component, class_name: 'Product'
 
+  #---
+  validates :quantity, numericality: {only_integer: true, greater_than: 0}
+  validates :component_id, presence: true
 end
