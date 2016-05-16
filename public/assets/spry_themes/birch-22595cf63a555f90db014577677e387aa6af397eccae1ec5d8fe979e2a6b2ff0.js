db2aabb2a5cec34bb796f4e8ceb0f2a85ebe3789
Sami Rosenblad <rosenblad@gmail.com>
2016-05-16 10:53:37 +0300

    New assets

---
+++ public/assets/spry_themes/birch-22595cf63a555f90db014577677e387aa6af397eccae1ec5d8fe979e2a6b2ff0.js
@@ -0,0 +1,1 @@
+(function(){jQuery(function(){return $("#categories-wrap").make_room($("#categories-wrap > .categories > ul > li.category.active > .subcategories-wrap")),$(document).on("page:done",function(e,r,a,o,t){return $("#categories-wrap").make_room($("#categories-wrap > .categories > ul > li.category.active > .subcategories-wrap"))})})}).call(this);
