127cbeb5e821b585994ab80749982f5e64c5338a
Sami Rosenblad <rosenblad@gmail.com>
2016-07-08 10:37:28 +0300

    Added support for parallax scrolling elements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,6 +32,7 @@
 //= require unobtrusive_flash_bootstrap
 //= require wiselinks
 //= require nprogress
+//= require ./parallax.min.js
 //= require ./numbro.min.js
 //= require ./languages.min.js
 //= require ./jquery.smartmenus.js
