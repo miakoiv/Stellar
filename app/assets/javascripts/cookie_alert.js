7129d4a66cfd230f69b63b28c18f6b2658ee12ae
Sami Rosenblad <rosenblad@gmail.com>
2016-08-09 10:07:42 +0300

    Restyled the cookie alert and made sure it's positioned on top of other elements, including the tawk.to widget

--- app/assets/javascripts/cookie_alert.js
+++ app/assets/javascripts/cookie_alert.js
@@ -1,12 +1,10 @@
 $(function(){
 
-  $("#cookie_alert.secondary-alert").on( 'mouseenter', function(e){
-    $("#cookie_alert_text").show({effect: 'slide', duration: 500});
-
+  $("#cookie_alert.secondary-alert").on('mouseenter', function(e){
+    $("#cookie_alert_text").show({effect: 'fade', duration: 500});
   });
 
-  $("#cookie_alert.secondary-alert").on( 'mouseleave', function(e){
-    $("#cookie_alert_text").hide({effect: 'slide', duration: 250});
-
+  $("#cookie_alert.secondary-alert").on('mouseleave', function(e){
+    $("#cookie_alert_text").hide({effect: 'fade', duration: 250});
   });
 });
