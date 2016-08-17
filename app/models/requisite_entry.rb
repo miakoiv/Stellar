72ccf6dfaad1fa8b9f15af5743685b55a1b82f90
Sami Rosenblad <rosenblad@gmail.com>
2016-08-17 09:37:32 +0300

    Added a default scope to requisite entries to have them sorted by priority

--- app/models/requisite_entry.rb
+++ app/models/requisite_entry.rb
@@ -7,6 +7,8 @@ class RequisiteEntry < ActiveRecord::Base
   belongs_to :product, touch: true
   belongs_to :requisite, class_name: 'Product'
 
+  default_scope { sorted }
+
   #---
   validates :requisite_id, presence: true
 end
