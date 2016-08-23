b3b86b7a260ea9062aaeb36e8990fcaf478bc097
Sami Rosenblad <rosenblad@gmail.com>
2016-08-23 14:21:39 +0300

    Redesigned most of the storefront views with new DOM structure, better parallax window support, and supporting stylesheets

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -13,12 +13,13 @@ module StoreHelper
 
   # Pretty-prints a hash from Product#stock.
   def product_stock_string(hash)
-    hash.values.map do |i|
-      content_tag(:span, class: i.klass, title: i.title, data: {toggle: 'tooltip'}) do
-        content_tag(:span, i.adjustment == 0 ? i.amount || 0 :
-          sprintf("%i(%+i)", i.amount || 0, i.adjustment))
-      end
-    end.join.html_safe
+    content_tag(:ul) do
+      hash.values.map do |i|
+        content_tag(:li, class: i.klass) do
+          content_tag(:span, i.adjustment == 0 ? i.amount || 0 : sprintf("%i(%+i)", i.amount || 0, i.adjustment)) + i.title
+        end
+      end.join.html_safe
+    end
   end
 
   def property_string(product_properties)
