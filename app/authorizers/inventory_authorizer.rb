37f500ec0a83274ee9e6308b99134671324a417b
Sami Rosenblad <rosenblad@gmail.com>
2016-09-13 14:11:48 +0300

    Introduced a new role inventory_manage that allows creating, editing, deleting inventories, and eventually creating inventory entries

--- app/authorizers/inventory_authorizer.rb
+++ app/authorizers/inventory_authorizer.rb
@@ -3,22 +3,18 @@
 class InventoryAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
-    user.has_cached_role?(:superuser) ||
-    user.has_cached_role?(:store_admin)
+    user.has_cached_role?(:inventory_manage)
   end
 
   def self.readable_by?(user)
-    user.has_cached_role?(:superuser) ||
-    user.has_cached_role?(:store_admin)
+    user.has_cached_role?(:inventory_manage)
   end
 
   def self.updatable_by?(user)
-    user.has_cached_role?(:superuser) ||
-    user.has_cached_role?(:store_admin)
+    user.has_cached_role?(:inventory_manage)
   end
 
   def self.deletable_by?(user)
-    user.has_cached_role?(:superuser) ||
-    user.has_cached_role?(:store_admin)
+    user.has_cached_role?(:inventory_manage)
   end
 end
