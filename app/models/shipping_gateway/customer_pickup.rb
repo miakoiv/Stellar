251a844ca0f8e0e5e9c13bec58c4ad7e4b2f015e
Sami Rosenblad <rosenblad@gmail.com>
2016-09-27 11:35:06 +0300

    Added model and view for a simple shipping gateway with no internal logic (customer pick-up)

---
+++ app/models/shipping_gateway/customer_pickup.rb
@@ -0,0 +1,19 @@
+#encoding: utf-8
+
+module ShippingGateway
+
+  class CustomerPickup
+    include ActiveModel::Model
+
+    attr_accessor :order
+
+    def initialize(attributes = {})
+      super
+      raise ArgumentError if order.nil?
+    end
+
+    def to_partial_path
+      'shipping_gateway/customer_pickup'
+    end
+  end
+end
