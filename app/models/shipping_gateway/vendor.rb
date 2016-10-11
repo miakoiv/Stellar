36d9e9c057754440a2078733b211eac5218877fc
Sami Rosenblad <rosenblad@gmail.com>
2016-10-11 11:07:16 +0300

    Added a shipping gateway for vendor arranged shipments

---
+++ app/models/shipping_gateway/vendor.rb
@@ -0,0 +1,19 @@
+#encoding: utf-8
+
+module ShippingGateway
+
+  class Vendor
+    include ActiveModel::Model
+
+    attr_accessor :order
+
+    def initialize(attributes = {})
+      super
+      raise ArgumentError if order.nil?
+    end
+
+    def to_partial_path
+      'shipping_gateway/vendor'
+    end
+  end
+end
