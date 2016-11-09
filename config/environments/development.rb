36b20bd43023c9997341cdc7d64945c915692c63
Sami Rosenblad <rosenblad@gmail.com>
2016-11-09 14:12:14 +0200

    Added a config option to disable throwing exceptions for missing assets, working around a long standing issue with sprockets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Do not raise exceptions for missing assets.
+  config.assets.check_precompiled_asset = false
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
