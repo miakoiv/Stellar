896e90bb6649d9768db886c3838452f84036b18d
Sami Rosenblad <rosenblad@gmail.com>
2016-12-09 09:51:22 +0200

    Renamed hostname attribute portal to is_subdomain

--- db/migrate/20161208121544_create_hostnames.rb
+++ db/migrate/20161208121544_create_hostnames.rb
@@ -3,7 +3,7 @@ class CreateHostnames < ActiveRecord::Migration
     create_table :hostnames do |t|
       t.references :resource, polymorphic: true, index: true
       t.string :fqdn, null: false, index: true
-      t.boolean :portal, null: false, default: false
+      t.boolean :is_subdomain, null: false, default: false
 
       t.timestamps null: false
     end
