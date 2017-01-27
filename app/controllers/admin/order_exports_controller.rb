2d167ec594e2017272839435901b9fa7480ddae4
Sami Rosenblad <rosenblad@gmail.com>
2017-01-27 13:48:33 +0200

    Created a new AdminOrderExportsController that has no routes pointed to it, to handle XML renders of orders

---
+++ app/controllers/admin/order_exports_controller.rb
@@ -0,0 +1,15 @@
+#encoding: utf-8
+#
+# This controller only handles a single task: rendering orders
+# in XML format using Builder. No routes point to this controller,
+# hence rendering happens by calling #render.
+#
+class Admin::OrderExportsController < ApplicationController
+
+  # GET /admin/order_exports/1.xml
+  def show
+    respond_to do |format|
+      format.xml
+    end
+  end
+end
