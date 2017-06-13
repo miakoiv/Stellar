6d54994bae620a060d4c90b3cf55f8cc5ce4c25a
Sami Rosenblad <rosenblad@gmail.com>
2017-06-13 16:53:52 +0300

    New assets

---
+++ public/assets/spry_themes/carbon-7a73d3fd57761505ee06df66fbdb9182608cdd6ba9bb6da8f1ea1bd2e16a688b.js
@@ -0,0 +1,1 @@
+(function(){this.carbon_init=function(){return $("#masthead").css("height",$(window).height()),$("#main-nav").stick_in_parent({offset_top:-30})},jQuery(function(){return carbon_init(),$(document).on("page:done",carbon_init)})}).call(this);
