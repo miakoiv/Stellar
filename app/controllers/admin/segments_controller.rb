affb39b17629b1d396604fbfabe8aa4db22772a6
Sami Rosenblad <rosenblad@gmail.com>
2017-07-21 17:00:13 +0300

    Added controller, views, authorizer, and l10n entries for segments

---
+++ app/controllers/admin/segments_controller.rb
@@ -0,0 +1,42 @@
+#encoding: utf-8
+
+class Admin::SegmentsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_segment, only: [:edit, :update]
+
+  authorize_actions_for Segment
+
+  # No layout, this controller never renders HTML.
+
+  # GET /admin/segments/1/edit.js
+  def edit
+    respond_to :js
+  end
+
+  # PATCH/PUT /admin/segments/1.js
+  def update
+    respond_to do |format|
+      if @segment.update(segment_params)
+        format.js
+      else
+        format.js { render json: @segment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_segment
+      @segment = Segment.joins(:section).find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def segment_params
+      params.require(:segment).permit(
+        :template, :resource_id, :resource_type,
+        :headline, :subhead, :body,
+        :map_latitude, :map_longitude
+      )
+    end
+end
