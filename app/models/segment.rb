1050ec06e45a56d9db0bb883d8cc488ed8ab8296
Sami Rosenblad <rosenblad@gmail.com>
2017-07-31 17:47:17 +0300

    Added placeholders for image and text column sections, updated Segment#fill_default_content accordingly, removed decrepit l10n entries

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -13,6 +13,9 @@ class Segment < ActiveRecord::Base
   include Reorderable
 
   #---
+  LOREM_IPSUM = '<p><font color="#cec6ce">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</font></p>'
+
+  #---
   enum template: {
     empty: 0,
     column: 1,
@@ -52,9 +55,6 @@ class Segment < ActiveRecord::Base
 
   private
     def fill_default_content
-      defaults = I18n.t("admin.segments.defaults.#{template}")
-      if defaults.is_a?(Hash)
-        self.attributes = defaults
-      end
+      self.body = LOREM_IPSUM if column?
     end
 end
