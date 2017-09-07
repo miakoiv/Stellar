4d80525bf3ec8efbf32a218be1db93b9ffdda216
Sami Rosenblad <rosenblad@gmail.com>
2017-09-07 13:58:35 +0300

    Fixed issues with the hostname migration not resetting column information and losing data

--- db/migrate/20170905122049_change_resource_to_store_in_hostnames.rb
+++ db/migrate/20170905122049_change_resource_to_store_in_hostnames.rb
@@ -1,8 +1,9 @@
 class ChangeResourceToStoreInHostnames < ActiveRecord::Migration
   def up
     add_reference :hostnames, :store, null: false, index: true, first: true
+    Hostname.reset_column_information
     Hostname.where(resource_type: 'Store').each do |hostname|
-      hostname.update store_id: hostname.resource_id
+      hostname.update_columns store_id: hostname.resource_id
     end
     remove_index :hostnames, [:resource_type, :resource_id]
     remove_reference :hostnames, :resource, polymorphic: true
@@ -10,8 +11,9 @@ class ChangeResourceToStoreInHostnames < ActiveRecord::Migration
 
   def down
     add_reference :hostnames, :resource, polymorphic: true, index: true, first: true
+    Hostname.reset_column_information
     Hostname.all.each do |hostname|
-      hostname.update resource: Store.find(hostname.store_id)
+      hostname.update_columns resource_type: 'Store', resource_id: hostname.store_id
     end
     remove_index :hostnames, :store_id
     remove_reference :hostnames, :store
