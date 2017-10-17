bf60e9c051a322e8344b02ec0b6bb867b829db7d
Sami Rosenblad <rosenblad@gmail.com>
2017-10-17 17:00:01 +0300

    Fixed issues with markup and margin calculations for alternate prices by adding an argument for a group to use as a pricing context

--- app/models/alternate_price.rb
+++ app/models/alternate_price.rb
@@ -15,17 +15,15 @@ class AlternatePrice < ActiveRecord::Base
     group.present? && group.modifier || nil
   end
 
-  # Markup percentage from group base price to this alternate price.
-  def markup_percent
-    return nil if group.nil?
+  # Markup percentage calculated for given group from their base price.
+  def markup_percent(group)
     base_price = product.send(group.price_method)
     return nil if base_price.nil? || price.nil? || base_price.zero?
     100 * (price - base_price) / base_price
   end
 
-  # Margin percentage between group base price and this alternate price.
-  def margin_percent
-    return nil if group.nil?
+  # Margin percentage for given group.
+  def margin_percent(group)
     base_price = product.send(group.price_method)
     return nil if base_price.nil? || price.nil? || price.zero?
     100 * (price - base_price) / price
