b86570f1718aa0772128561bebdc3ab863cdc70e
Sami Rosenblad <rosenblad@gmail.com>
2018-01-16 10:27:46 +0200

    Hooked up columns controller to allow updating columns through the layout editor controls

---
+++ app/authorizers/column_authorizer.rb
@@ -0,0 +1,8 @@
+#encoding: utf-8
+
+class ColumnAuthorizer < ApplicationAuthorizer
+
+  def self.default(able, user, opts)
+    user.has_cached_role?(:page_editor, opts[:at])
+  end
+end
---
+++ app/controllers/admin/columns_controller.rb
@@ -0,0 +1,35 @@
+#encoding: utf-8
+
+class Admin::ColumnsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_column
+
+  # No layout, this controller never renders HTML.
+
+  # PATCH/PUT /admin/columns/1.js
+  def update
+    authorize_action_for @column, at: current_store
+
+    respond_to do |format|
+      if @column.update(column_params)
+        format.js
+      else
+        format.js { render json: @column.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_column
+      @column = Column.joins(:section).find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def column_params
+      params.require(:column).permit(
+        :alignment, :pivot
+      )
+    end
+end
