adb7042a85654a6866aee5aa0a293e7655cc2c80
Sami Rosenblad <rosenblad@gmail.com>
2018-01-17 16:40:45 +0200

    Removed the jumbotron width option for sections, jumbotrons are now applied to segments

--- app/models/section.rb
+++ app/models/section.rb
@@ -28,7 +28,7 @@ class Section < ActiveRecord::Base
 
   # Available content widths defined in layouts.css.
   WIDTHS = %w{
-    jumbotron spread col-12 col-10 col-8 col-6
+    spread col-12 col-10 col-8 col-6
   }.freeze
 
   #---
