9b709614316289cf7afd757edb729a5cab71b883
Sami Rosenblad <rosenblad@gmail.com>
2018-03-06 11:01:13 +0200

    Reversed the previous change to allow nil lot codes on tranfer items, any items created automatically must employ logic to pick their lot codes from existing stock

--- app/models/transfer_item.rb
+++ app/models/transfer_item.rb
@@ -9,13 +9,13 @@ class TransferItem < ActiveRecord::Base
 
   #---
   belongs_to :transfer
-  delegate :source, :destination, to: :transfer
+  delegate :complete?, :source, :destination, to: :transfer
 
   belongs_to :product
 
   #---
   validates :product_id, presence: true
-  validates :lot_code, presence: true, unless: :shipment?
+  validates :lot_code, presence: true
   validates :amount, numericality: {
     integer_only: true,
     greater_than_or_equal_to: 1
@@ -36,14 +36,7 @@ class TransferItem < ActiveRecord::Base
     source_item.present? && source_item.available >= amount
   end
 
-  # Checks if this item is part of a transfer associated with a shipment,
-  # allowing lot code to be omitted, and automatically resolved to pick
-  # the oldest available lot when the transfer is being completed.
-  def shipment?
-    transfer.shipment.present?
-  end
-
   def appearance
-    feasible? || 'danger text-danger'
+    complete? || feasible? || 'danger text-danger'
   end
 end
