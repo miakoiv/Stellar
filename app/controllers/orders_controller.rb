987a860a9844e97ed2ce84effd1e5bb9689e41cb
Sami Rosenblad <rosenblad@gmail.com>
2018-03-22 11:47:25 +0200

    Added a predefined search param to orders to limit the order history view to the current store

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -109,6 +109,6 @@ class OrdersController < ApplicationController
 
     # The search is limited to the current user's personal history.
     def search_params
-      @query.merge(user_id: current_user.id)
+      @query.merge(store: current_store, user_id: current_user.id)
     end
 end
