b0046bf89f2ac201fa568c8da1dda104e85b9626
Sami Rosenblad <rosenblad@gmail.com>
2018-04-20 15:44:26 +0300

    Greatly simplified order mailer previews

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -4,36 +4,12 @@
 
 class OrderMailerPreview < ActionMailer::Preview
 
-  Store.all.each do |store|
-    store.orders.complete.last(10).each do |order|
-      if order.has_payment?
-        define_method "#{store} #{order} receipt" do
-          order.email(:receipt, order.customer_string)
-        end
-        define_method "#{store} #{order} processing" do
-          order.email(:processing, order.customer_string, nil, bcc: false)
-        end
-      else
-        define_method "#{store} #{order} acknowledge" do
-          order.email(:acknowledge, order.customer_string)
-        end
-        define_method "#{store} #{order} confirmation" do
-          order.email(:confirmation, order.customer_string, nil, bcc: false)
-        end
-      end
-      define_method "#{store} #{order} shipment" do
-        order.email(:shipment, order.customer_string, nil, bcc: false)
-      end
-      define_method "#{store} #{order} cancellation" do
-        order.email(:cancellation, order.customer_string)
-      end
-      order.items_by_vendor.each do |vendor, items|
-        vendor.notified_users.each do |user|
-          define_method "#{store} #{order} notify vendor #{user.name}" do
-            order.email(:notification, user.to_s, items, bcc: false, pricing: false)
-          end
-        end
-      end
+  [:receipt, :acknowledge, :processing, :confirmation, :shipment, :notification, :cancellation].each do |type|
+    define_method type do
+      order = Order.concluded.sample
+      to = order.customer_string
+      options = {bcc: true, pricing: true}
+      OrderMailer.send(type, order, to, nil, options)
     end
   end
 end
