6e77771863d0604203732c624580bb745e90944d
Sami Rosenblad <rosenblad@gmail.com>
2018-05-03 15:41:26 +0300

    Updated the SmartPOST gateway to similar terminology as the new Pakettikauppa gateway is using

--- app/models/shipping_gateway/smart_post.rb
+++ app/models/shipping_gateway/smart_post.rb
@@ -35,21 +35,21 @@ module ShippingGateway
       base_price
     end
 
-    # Performs a lookup of SmartPost pickup locations by given zip code.
-    # The post office code servicing the zip area is looked up first,
+    # Performs a lookup of SmartPost pickup locations by given postal code.
+    # The post office code servicing the postal code area is looked up first,
     # using its locations to look for the nearest five locations around it.
     # Returns a tuple of both results.
-    def smartpost_lookup(zipcode)
-      postcode = @pickup_connector.lookup(zipcode: zipcode).parsed_response[0]
-      return [nil, nil] if postcode.nil?
+    def smartpost_lookup(postalcode)
+      areacode = @pickup_connector.lookup(zipcode: postalcode).parsed_response[0]
+      return [nil, nil] if areacode.nil?
       query = {
         type: 'smartpost',
-        longitude: postcode['MapLongitude'],
-        latitude: postcode['MapLatitude'],
+        longitude: areacode['MapLongitude'],
+        latitude: areacode['MapLatitude'],
         top: 5
       }
       locations = @pickup_connector.lookup(query).parsed_response
-      return [postcode, locations]
+      return [areacode, locations]
     end
 
     def to_partial_path
