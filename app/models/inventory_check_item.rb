bf8c375896461bc49da6ccff1535b6623657edd4
Sami Rosenblad <rosenblad@gmail.com>
2018-08-27 12:19:35 +0300

    Updated the inventory check item model with delegations and lot code/serial handling

--- app/models/inventory_check_item.rb
+++ app/models/inventory_check_item.rb
@@ -7,19 +7,43 @@ class InventoryCheckItem < ActiveRecord::Base
 
   #---
   belongs_to :inventory_check
-  belongs_to :product
 
   # Inventory check items have a product association and
   # attributes for lot code, expiration, and on hand amount,
   # but may be associated with a matching inventory item.
   belongs_to :inventory_item
 
+  belongs_to :product
+  delegate :real?, to: :product
+  delegate :code, :customer_code, :title, :subtitle, to: :product, prefix: true
+
   #---
-  validates :inventory_check_id, presence: true
   validates :product_id, presence: true
   validates :lot_code, presence: true
   validates :amount, numericality: {
     integer_only: true,
     greater_than_or_equal_to: 1
   }
+
+  attr_accessor :serial
+  before_validation :concatenate_lot_code
+
+  #---
+  def appearance
+  end
+
+  def customer_code=(val)
+    self.product = Product.find_by(customer_code: val)
+  end
+
+  def to_s
+    "%d⨉ %s (%s)" % [amount, product, lot_code]
+  end
+
+  private
+    # Lot code and serial are joined by hyphen, either one appearing alone
+    # is used as the lot code.
+    def concatenate_lot_code
+      self[:lot_code] = [lot_code, serial].map(&:presence).compact.join('-')
+    end
 end
