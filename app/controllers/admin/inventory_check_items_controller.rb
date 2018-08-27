f10b5b1b6876e9c660d64d8865745fcaef881e56
Sami Rosenblad <rosenblad@gmail.com>
2018-08-27 12:20:32 +0300

    Added controller and views to create/update/delete inventory check items

--- app/controllers/admin/inventory_check_items_controller.rb
+++ app/controllers/admin/inventory_check_items_controller.rb
@@ -1,2 +1,60 @@
+#encoding: utf-8
+
 class Admin::InventoryCheckItemsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_inventory_check, only: [:create]
+
+  # No layout, this controller never renders HTML.
+
+  # POST /admin/inventory_checks/1/inventory_check_items
+  def create
+    @inventory_check_item = @inventory_check.inventory_check_items.build(inventory_check_item_params)
+
+    respond_to do |format|
+      if @inventory_check_item.save
+        track @inventory_check_item, @inventory_check
+        format.js { render :create }
+      else
+        format.js { render :error }
+      end
+    end
+  end
+
+  # PATCH/PUT /admin/inventory_check_items/1
+  def update
+    @inventory_check_item = InventoryCheckItem.find(params[:id])
+    @inventory_check = @inventory_check_item.inventory_check
+
+    respond_to do |format|
+      if @inventory_check_item.update(inventory_check_item_params)
+        track @inventory_check_item, @inventory_check
+        format.js { render :update }
+      else
+        format.js { render :error }
+      end
+    end
+  end
+
+  # DELETE /admin/inventory_check_items/1
+  def destroy
+    @inventory_check_item = InventoryCheckItem.find(params[:id])
+    @inventory_check = @inventory_check_item.inventory_check
+    track @inventory_check_item, @inventory_check
+
+    @inventory_check_item.destroy
+  end
+
+  private
+    def set_inventory_check
+      @inventory_check = current_store.inventory_checks.find(params[:inventory_check_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def inventory_check_item_params
+      params.require(:inventory_check_item).permit(
+        :product_id, :lot_code, :expires_at, :amount,
+        :customer_code, :serial
+      )
+    end
 end
