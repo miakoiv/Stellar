21ab0e329a7531d5572c028006ef5412aa143b16
Sami Rosenblad <rosenblad@gmail.com>
2018-10-15 12:56:58 +0300

    Changed order item search base query to reorder on order completion timestamp, added several methods that are shared with order report row searches

--- app/models/order_item_search.rb
+++ app/models/order_item_search.rb
@@ -8,7 +8,15 @@ class OrderItemSearch < Searchlight::Search
 
   def base_query
     OrderItem.joins(:order).merge(Order.approved)
-      .order('order_items.created_at')
+      .reorder('orders.completed_at')
+  end
+
+  def options
+    this_month = Date.current.all_month
+    super.tap do |opts|
+      opts[:since_date] = date_param(raw_options[:since_date], this_month.first)
+      opts[:until_date] = date_param(raw_options[:until_date], this_month.last)
+    end
   end
 
   def search_store
@@ -23,7 +31,30 @@ class OrderItemSearch < Searchlight::Search
     query.where(orders: {order_type_id: order_type})
   end
 
+  def search_store_portal_id
+    query.where(orders: {store_portal_id: store_portal_id})
+  end
+
+  def search_shipping_country_code
+    query.where(orders: {shipping_country_code: shipping_country_code})
+  end
+
   def search_product_id
     query.where(product_id: product_id)
   end
+
+  def search_since_date
+    query.where('DATE(orders.completed_at) >= ?', since_date)
+  end
+
+  def search_until_date
+    query.where('DATE(orders.completed_at) <= ?', until_date)
+  end
 end
+
+private
+  def date_param(param, default)
+    return default unless param.present?
+    return param if param.is_a?(Date)
+    Date.parse(param)
+  end
