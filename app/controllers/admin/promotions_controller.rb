miakoiv/Stellar

commit 51f74bdd1bd2b52c6467e433abe06db62e6c9739

Added a default price attribute to promotion handlers to allow setting a default price for items added to vanilla promotions

app/controllers/admin/promotions_controller.rb
@@ -97,9 +97,10 @@ class Admin::PromotionsController < ApplicationController
     }
     ActiveRecord::Base.transaction do
       product_ids.each do |product_id|
-        @promotion.promoted_items.find_or_create_by(
-          product: Product.find(product_id)
-        )
+        @promotion.promoted_items
+          .create_with(
+            price_cents: @promotion.promotion_handler.default_price_cents
+          ).find_or_create_by(product: Product.find(product_id))
       end
     end
     respond_to :js
@@ -118,9 +119,10 @@ class Admin::PromotionsController < ApplicationController
       category_ids.each do |category_id|
         category = Category.find(category_id)
         category.products.live.each do |product|
-          @promotion.promoted_items.find_or_create_by(
-            product: product
-          )
+          @promotion.promoted_items
+            .create_with(
+              price_cents: @promotion.promotion_handler.default_price_cents
+            ).find_or_create_by(product: Product.find(product_id))
         end
       end
     end
@@ -139,8 +141,9 @@ class Admin::PromotionsController < ApplicationController
         :name, :group_id, :promotion_handler_type,
         :first_date, :last_date,
         promotion_handler_attributes: [
-          :id, :description,
-          :order_total, :required_items, :items_total, :discount_percent
+          :id, :description, :default_price,
+          :order_total, :required_items, :items_total,
+          :discount_percent
         ]
       )
     end

app/models/promotion_handler/vanilla.rb
@@ -3,6 +3,9 @@
 class PromotionHandler
   class Vanilla < PromotionHandler
 
+    monetize :default_price_cents, allow_nil: true, numericality: {greater_than: 0}
+
+    #---
     # Vanilla promotions do not apply adjustments since order items
     # are created with the promoted price.
     def apply!(order, items)

app/views/admin/promotion_handler/_vanilla.html.haml
@@ -1,0 +1,1 @@
+= f.text_field :default_price, append: '€', class: 'text-right'

config/locales/models-en.yml
@@ -455,6 +455,8 @@ en:
       promotion_handler/freebie_bundle:
         required_items: Number of items
         discount_percent: Apply discount
+      promotion_handler/vanilla:
+        default_price: Default price
       property:
         value_type: Type
         measurement_unit_id: Measurement unit

config/locales/models-fi.yml
@@ -455,6 +455,8 @@ fi:
       promotion_handler/freebie_bundle:
         required_items: Tuotteiden määrä
         discount_percent: Alenna hintaa
+      promotion_handler/vanilla:
+        default_price: Oletushinta
       property:
         value_type: Tyyppi
         measurement_unit_id: Mittayksikkö

db/migrate/20180928135248_add_default_price_cents_to_promotion_handlers.rb
@@ -1,0 +1,5 @@
+class AddDefaultPriceCentsToPromotionHandlers < ActiveRecord::Migration
+  def change
+    add_column :promotion_handlers, :default_price_cents, :integer, after: :description
+  end
+end

db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928061121) do
+ActiveRecord::Schema.define(version: 20180928135248) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "store_id",      limit: 4,     null: false
@@ -649,15 +649,16 @@ ActiveRecord::Schema.define(version: 20180928061121) do
   add_index "promoted_items", ["promotion_id"], name: "index_promoted_items_on_promotion_id", using: :btree
 
   create_table "promotion_handlers", force: :cascade do |t|
-    t.integer  "promotion_id",      limit: 4,   null: false
-    t.string   "type",              limit: 255, null: false
-    t.string   "description",       limit: 255
-    t.integer  "order_total_cents", limit: 4
-    t.integer  "required_items",    limit: 4
-    t.integer  "items_total_cents", limit: 4
-    t.integer  "discount_percent",  limit: 4
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.integer  "promotion_id",        limit: 4,   null: false
+    t.string   "type",                limit: 255, null: false
+    t.string   "description",         limit: 255
+    t.integer  "default_price_cents", limit: 4
+    t.integer  "order_total_cents",   limit: 4
+    t.integer  "required_items",      limit: 4
+    t.integer  "items_total_cents",   limit: 4
+    t.integer  "discount_percent",    limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   add_index "promotion_handlers", ["promotion_id"], name: "index_promotion_handlers_on_promotion_id", using: :btree

commit 354530971a98726ee09ac83e1dd0457b6abe9872

Fixed a regression in adding categories to a promotion, introduced in 51f74bdd1bd2b52c6467e433abe06db62e6c9739

app/controllers/admin/promotions_controller.rb
@@ -122,7 +122,7 @@ class Admin::PromotionsController < ApplicationController
           @promotion.promoted_items
             .create_with(
               price_cents: @promotion.promotion_handler.default_price_cents
-            ).find_or_create_by(product: Product.find(product_id))
+            ).find_or_create_by(product: product)
         end
       end
     end