c426eecb0bbf1d237f8bc03b7b4cb6aaa7fcfaf3
Sami Rosenblad <rosenblad@gmail.com>
2018-11-16 13:48:30 +0200

    Added proper before_action callbacks to show_category_order

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -14,7 +14,7 @@ class StoreController < ApplicationController
   # Unauthenticated guests may visit the store.
   before_action :authenticate_user_or_skip!, except: [:index, :show_page]
 
-  with_options only: [:front, :cart, :show_category, :show_department, :show_page, :show_product, :show_promotion, :show_tag] do
+  with_options only: [:front, :cart, :show_category, :show_category_order, :show_department, :show_page, :show_product, :show_promotion, :show_tag] do
     before_action :set_categories
     before_action :set_departments
   end
