fb3f62e5ed3a474f560868374ab380b50b59020d
Sami Rosenblad <rosenblad@gmail.com>
2018-11-19 11:50:05 +0200

    Rearranged the section form widgets and removed the name field since it was not used for anything

--- app/controllers/admin/sections_controller.rb
+++ app/controllers/admin/sections_controller.rb
@@ -86,7 +86,7 @@ class Admin::SectionsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def section_params
       params.fetch(:section) {{}}.permit(
-        :name, :width, :layout, :gutters, :viewport,
+        :width, :layout, :gutters, :viewport,
         :background_color, :fixed_background,
         :gradient_color, :gradient_type, :gradient_direction, :gradient_balance
       )
