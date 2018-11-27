d41c135983c9ca09f36d6eb83cd95d85eaf4a93b
Sami Rosenblad <rosenblad@gmail.com>
2018-11-27 11:58:49 +0200

    Replaced segment placeholder partials with a helper that renders a segment template image as a placeholder

--- app/helpers/admin/segments_helper.rb
+++ app/helpers/admin/segments_helper.rb
@@ -1,2 +1,9 @@
 module Admin::SegmentsHelper
+  def segment_placeholder(segment)
+    content_tag(:div, class: 'placeholder') do
+      content_tag(:figure) do
+        image_tag "templates/#{segment.template}.svg"
+      end
+    end
+  end
 end
