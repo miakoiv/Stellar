a2e38b8bf9be8fede656cf7a680a899f6d43f55f
Sami Rosenblad <rosenblad@gmail.com>
2019-02-13 17:52:30 +0200

    Removed deprecated raise_in_transactional_callbacks config setting

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module Stellar
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fi
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Let Rails routing handle error conditions
     config.exceptions_app = self.routes
 
