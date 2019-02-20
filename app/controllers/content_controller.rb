5b646d74dffb0ce62b3ba1740d8a22da32b20931
Sami Rosenblad <rosenblad@gmail.com>
2019-02-20 13:15:14 +0200

    Fixed unpermitted params issue in content controller, and enable caching on the ajax call in content feed segments

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -4,13 +4,14 @@ class ContentController < ApplicationController
 
   # GET /content/feed
   def feed
-    @id = params[:id]
-    store = Store.find(params[:store])
-    paginates_per = params[:items].to_i
-    content_class = params[:contentClass].classify
+    p = content_params
+    @id = p[:id]
+    store = Store.find(p[:store])
+    paginates_per = p[:items].to_i
+    content_class = p[:contentClass].classify
     gateway_class = "ContentGateway::#{content_class}".constantize
-    items, count = gateway_class.new(store).feed(params)
-    @items = Kaminari.paginate_array(items, total_count: count).page(params[:page]).per(paginates_per)
+    items, count = gateway_class.new(store).feed(p)
+    @items = Kaminari.paginate_array(items, total_count: count).page(p[:page]).per(paginates_per)
 
     respond_to do |format|
       format.html
@@ -20,4 +21,10 @@ class ContentController < ApplicationController
     @error_message = e.message
     logger.warn @error_message
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def content_params
+      params.permit(:id, :store, :items, :contentClass, :contentType, :page)
+    end
 end
