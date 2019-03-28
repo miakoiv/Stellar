fb5cd15e211e6f69f75be076ee1ef80225277f96
Sami Rosenblad <rosenblad@gmail.com>
2019-03-28 11:45:11 +0200

    Added the concept of address inheritance to the addressed model and methods to query the inherited addresses, added the UI components to the address panel to show inherited addresses when applicable

--- app/models/concerns/addressed.rb
+++ app/models/concerns/addressed.rb
@@ -11,6 +11,25 @@ module Addressed
     accepts_nested_attributes_for :shipping_address
   end
 
+  # Addressed models have address inheritance if they recognize parents.
+  def has_address_inheritance?
+    respond_to?(:parent)
+  end
+
+  def inherited_billing_address
+    billing_address.presence ||
+      has_address_inheritance? && parent&.inherited_billing_address
+  end
+
+  def inherited_shipping_address
+    shipping_address.presence ||
+      has_address_inheritance? && parent&.inherited_shipping_address
+  end
+
+  def inherited_address(type)
+    send("inherited_#{type}_address")
+  end
+
   def update_address(type, address)
     raise ArgumentError unless type.in?(Addressed::ADDRESS_TYPES)
     update("#{type}_address" => address)
