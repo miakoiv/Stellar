77310dd7e541b3764a3f1dcd61322da7866bc692
Sami Rosenblad <rosenblad@gmail.com>
2019-04-25 13:13:37 +0300

    Updated group authorizer to prevent deleting groups that have any children, or there are associated orders

--- app/authorizers/group_authorizer.rb
+++ app/authorizers/group_authorizer.rb
@@ -16,7 +16,9 @@ class GroupAuthorizer < ApplicationAuthorizer
   end
 
   def deletable_by?(user, opts)
+    return false if resource.children_count > 0
     return false if resource.users.count > 0
+    return false if resource.orders_as_billing.any? || resource.orders_as_shipping.any?
     user.has_cached_role?(:superuser, opts[:at]) ||
     user.has_cached_role?(:group_editor, opts[:at])
   end
