b3ed64d55395de22f50ca817c6857440a714cf3d
Sami Rosenblad <rosenblad@gmail.com>
2019-06-07 09:27:51 +0300

    Added preliminary support for reporting a sale through the Kaunotar API, added comments

--- app/models/stock_gateway/kaunotar.rb
+++ app/models/stock_gateway/kaunotar.rb
@@ -12,6 +12,8 @@ module StockGateway
       @client_token = @store.stock_gateway_token
     end
 
+    # Queries the API for the stock level of `product`.
+    # Returns an integer, or 0 if the query fails.
     def stock(product)
       id = product.customer_code.presence || product.code
       begin
@@ -25,9 +27,37 @@ module StockGateway
       end
     end
 
+    # Sends an API call to report a sale based on `order` and its contents.
+    # Returns the argument if successful, nil otherwise.
+    def sale(order)
+      begin
+        response = self.class.post("/orders",
+          headers: headers,
+          body: sale_request(order)
+        ).parsed_response
+        return order
+      rescue => e
+        Rails.logger.error e.message
+        return nil
+      end
+    end
+
     private
       def headers
         {'X-USER-TOKEN' => @client_token}
+      end
+
+      def sale_request(order)
+        {
+          order_number: order.number,
+          timestamp: order.completed_at,
+          line_items: order.order_items.real.map { |item|
+            {
+              product: item.product.customer_code.presence || item.product.code,
+              amount: item.amount
+            }
+          }
+        }
       end
   end
 end
