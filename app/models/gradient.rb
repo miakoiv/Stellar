815240f25f10ec7f18e70a4c3c6d49800bca2c3e
Sami Rosenblad <rosenblad@gmail.com>
2019-08-21 12:30:17 +0300

    Reworked the gradient UI controls and merged the l10n entries under gradients

--- app/models/gradient.rb
+++ app/models/gradient.rb
@@ -1,20 +1,31 @@
 class Gradient
   include ActiveModel::Model
 
-  TYPES = %w{linear circle ellipse}.freeze
+  TYPES = [
+    ['solid', ''],
+    ['linear', 'linear'],
+    ['circle', 'circle'],
+    ['ellipse', 'ellipse']
+  ].freeze
 
   DIRECTIONS = [
-    'top left', 'top', 'top right',
-    'left', 'right',
-    'bottom left', 'bottom', 'bottom right'
+    ['↖', 'top left'],
+    ['↑', 'top'],
+    ['↗', 'top right'],
+    ['←', 'left'],
+    ['•', ''],
+    ['→', 'right'],
+    ['↙', 'bottom left'],
+    ['↓', 'bottom'],
+    ['↘', 'bottom right']
   ].freeze
 
   #---
   def self.type_options
-    TYPES.map { |g| [Gradient.human_attribute_value(:type, g), g] }
+    TYPES.map { |t, v| [Gradient.human_attribute_value(:type, t), v] }
   end
 
   def self.direction_options
-    DIRECTIONS.map { |g| [Gradient.human_attribute_value(:direction, g), g] }
+    DIRECTIONS
   end
 end
