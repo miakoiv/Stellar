fd90ce10d8d72e13d1ffc19fcff5c0ea694afc76
Sami Rosenblad <rosenblad@gmail.com>
2019-09-27 17:36:56 +0300

    Fixed an issue with an earlier migration attempting to update segments and triggering a callback that will fail because segments don't contain a border attribute yet

--- db/migrate/20190820103752_add_aspect_ratio_to_segments.rb
+++ db/migrate/20190820103752_add_aspect_ratio_to_segments.rb
@@ -6,7 +6,7 @@ class AddAspectRatioToSegments < ActiveRecord::Migration[5.2]
     # in reverse to turn 'shape-16-9' into '16:9'.
     ratios = Segment::SHAPES.map { |name, id| [id, name.split.first] }.to_h
     Segment.unscoped.where.not(shape: [nil, '']).each do |segment|
-      segment.update aspect_ratio: ratios[segment.shape]
+      segment.update_columns aspect_ratio: ratios[segment.shape]
     end
   end
 
