19aa9e8ba0e9da89c45799af273e6268aba58c18
Sami Rosenblad <rosenblad@gmail.com>
2019-10-18 12:11:08 +0300

    Updated the shipping method authorizer to prevent deletion of a shipping method when there are any shipments using it

--- app/authorizers/shipping_method_authorizer.rb
+++ app/authorizers/shipping_method_authorizer.rb
@@ -15,7 +15,8 @@ class ShippingMethodAuthorizer < ApplicationAuthorizer
     user.has_cached_role?(:store_admin, opts[:at])
   end
 
-  def self.deletable_by?(user, opts)
+  def deletable_by?(user, opts)
+    return false if resource.shipments.any?
     user.has_cached_role?(:superuser, opts[:at]) ||
     user.has_cached_role?(:store_admin, opts[:at])
   end
